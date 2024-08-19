3730e040e5277d486635fb7c3147019ae6cf2540
Lucas Aleh <lucasalehwork@gmail.com>
2024-08-19 11:33:49 +0700

    chore: Add registration functionality to AuthService

--- src/apps/auth/auth.service.ts
+++ src/apps/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { AccountEntity } from 'src/entities';
+import { BaseRepository } from 'src/repositories/base.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(AccountEntity)
+    private readonly accountRepository: BaseRepository<AccountEntity>,
+  ) {}
+  async register(body: any) {
+    const account = await this.accountRepository.findOneById(body.id);
+    return account;
+  }
+}
