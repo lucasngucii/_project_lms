2e359effad22722586c2301993fcec847a18f656
Lucas Aleh <lucasalehwork@gmail.com>
2024-08-19 11:34:07 +0700

    feat: Add findOneById method to BaseRepository

--- src/repositories/base.repository.ts
+++ src/repositories/base.repository.ts
@@ -1,8 +1,26 @@
+import { HttpException, HttpStatus, Logger } from '@nestjs/common';
+
 import { EntityManager, EntityTarget, Repository } from 'typeorm';
 
 export abstract class BaseRepository<T> extends Repository<T> {
+  protected logger: Logger = new Logger();
   constructor(target: EntityTarget<T>, entity: EntityManager) {
     super(target, entity);
   }
-  
+  async findOneById(id: string): Promise<T> {
+    try {
+      console.log('id', id);
+      const result = await this.findOneById(id);
+      console.log('result', result);
+      if (!result) {
+        throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+      }
+      return result;
+    } catch (error) {
+      throw new HttpException(
+        'Find One By Id Error',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 }
